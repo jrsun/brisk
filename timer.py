4927547d58191f2c229307db30b69baea2cf75bc
Jeffrey Sun <jsun@box.com>
2015-07-29 14:14:52 -0700

    investigated turn timeout

---
+++ timer.py
@@ -0,0 +1,14 @@
+import Brisk
+import time
+
+times = []
+SAMPLE_SIZE = 5
+POLL_TIME = 1 # seconds
+for i in xrange(SAMPLE_SIZE):
+	b = Brisk.Brisk()
+	start = time.time()
+	while not b.get_game_state()['winner']:
+		time.sleep(POLL_TIME)
+	end = time.time()
+	times.append(end - start)
+print times
