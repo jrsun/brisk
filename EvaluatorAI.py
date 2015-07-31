b595ab5fb92856cce1503031646d7a868167bd83
Jeffrey Sun <jsun@box.com>
2015-07-31 04:03:36 -0700

    working fortify evaluator

--- EvaluatorAI.py
+++ EvaluatorAI.py
@@ -1,11 +1,13 @@
 import AIBase
 import Reinforcement_Evaluator
+import Fortify_Evaluator
 
 class EvaluatorAI(AIBase.AIBase):
 
     def __init__(self):
         super(EvaluatorAI, self).__init__()
         self.re = Reinforcement_Evaluator.Reinforcement_Evaluator()
+        self.fe = Fortify_Evaluator.Fortify_Evaluator()
 
     def reinforce(self, num_reserves):
         ''' Returns {<territoryId>: <num troops deployed>} '''
@@ -28,10 +30,22 @@ class EvaluatorAI(AIBase.AIBase):
 
     def battle(self, legal_territories_to_attack):
         ''' Returns (attack, defend, num_armies_to_attack), or a None value if not attacking '''
+        pass
 
+    def _choose_best_battle(self, legal_territories_to_attack):
+        pass
 
     def fortify(self, legal_territories_to_fortify):
+        f, t, n = self._choose_best_fortify(legal_territories_to_fortify)
+        return (f['territory'], t['territory'], n)
         ''' Returns (from_territory, to_territory, num_armies_to_move), or a None value if ending turn '''
+
+    def _choose_best_fortify(self, legal_territories_to_fortify):
+        score = {}
+        for (src, dest) in legal_territories_to_fortify:
+            fortification = (src, dest, src['num_armies'] - 1)
+            score[self.fe.evaluate_action(fortification, self.map_layout, self.player_status, self.enemy_status)] = fortification
+        return score[max(score)]
 
 if __name__ == '__main__':
     bot = EvaluatorAI()
