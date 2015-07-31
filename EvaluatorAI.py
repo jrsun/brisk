4e3134fda56bcbb832a4bcdbeb05419b6cbe186d
Jeffrey Sun <jsun@box.com>
2015-07-31 04:31:41 -0700

    dummy battle() method works

--- EvaluatorAI.py
+++ EvaluatorAI.py
@@ -1,13 +1,19 @@
 import AIBase
 import Reinforcement_Evaluator
+import Battle_Evaluator
 import Fortify_Evaluator
+#debug
+import random
 
 class EvaluatorAI(AIBase.AIBase):
 
     def __init__(self):
         super(EvaluatorAI, self).__init__()
         self.re = Reinforcement_Evaluator.Reinforcement_Evaluator()
+        self.be = Battle_Evaluator.Battle_Evaluator()
         self.fe = Fortify_Evaluator.Fortify_Evaluator()
+
+    # what if there is no legal move?
 
     def reinforce(self, num_reserves):
         ''' Returns {<territoryId>: <num troops deployed>} '''
@@ -26,14 +32,21 @@ class EvaluatorAI(AIBase.AIBase):
         score = {}
         for territory in legal_territories_to_reinforce:
             score[self.re.evaluate_action(territory, self.map_layout, self.player_status, self.enemy_status)] = territory
-        return score[max(score)]['territory']
+        return score[max(score)]['territory'] if len(score) > 0 else None
 
     def battle(self, legal_territories_to_attack):
         ''' Returns (attack, defend, num_armies_to_attack), or a None value if not attacking '''
-        pass
+        print len(legal_territories_to_attack)
+        f, t, n = self._choose_best_battle(legal_territories_to_attack)
+        return (f['territory'], t['territory'], n)
 
     def _choose_best_battle(self, legal_territories_to_attack):
-        pass
+        score = {}
+        for (src, dest) in legal_territories_to_attack:
+            battle = (src, dest, min(3, src['num_armies'] - 1))
+            score[random.random()] = battle
+            # score[self.be.evaluate_action(attack, self.map_layout, self.player_status, self.enemy_status)] = battle
+        return score[max(score)] if len(score) > 0 else None
 
     def fortify(self, legal_territories_to_fortify):
         f, t, n = self._choose_best_fortify(legal_territories_to_fortify)
@@ -45,7 +58,7 @@ class EvaluatorAI(AIBase.AIBase):
         for (src, dest) in legal_territories_to_fortify:
             fortification = (src, dest, src['num_armies'] - 1)
             score[self.fe.evaluate_action(fortification, self.map_layout, self.player_status, self.enemy_status)] = fortification
-        return score[max(score)]
+        return score[max(score)] if len(score) > 0 else None
 
 if __name__ == '__main__':
     bot = EvaluatorAI()
